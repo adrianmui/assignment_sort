fe60fc99ceaf7f2eb552fc6c0d2da3b6bf5ccb41
jsteenb2 <jonathansteenbergen@gmail.com>
2016-07-15 10:10:22 -0700

    merge sort array

--- sort.rb
+++ sort.rb
@@ -1,5 +1,5 @@
 #sort.rb
-
+require 'pry'
 def insertion_sort(arr)
   sorted = []<< arr.shift
   until arr.empty?
@@ -24,11 +24,11 @@ def bubble_sort(arr)
   while i < arr.length - 1
     index_one = 0
     index_two = index_one + 1
-    
+
     (arr.length - 1 - i).times do
       if arr[index_one] > arr[index_two]
         #swaps
-        temp = arr[index_one] 
+        temp = arr[index_one]
         arr[index_one] = arr[index_two]
         arr[index_two] = temp
       end
@@ -41,9 +41,36 @@ def bubble_sort(arr)
 end
 
 
+def merge_sort(array)
+  # // if the array is one element long, just return it
+
+  # // mergeSort() the left half of the array
+
+  # // mergeSort() the right half of the array
+
+  # // merge() the two halves
+
+  # // return the merged array
+end
+
+def merge(leftArr, rightArr)
+  # // var newArr = [];
+  newArr = []
+  # // compare leftArr[0] and rightArr[0]
+  # // whichever is smaller, push it onto newArr
+  while leftArr.length + rightArr.length > 0
+    binding.pry
+    if  !(leftArr.empty?) && !(leftArr[0] < rightArr[0])
+      newArr << rightArr.shift
+    elsif !(rightArr.empty?) && !(leftArr[0] >= rightArr[0])
+      newArr << leftArr.shift
+    end
+  end
+  newArr
+end
 #testing
 
-
+p merge([1,2], [0, 3])
 
 #insertion_sort
 # p insertion_sort([1,3,2])
@@ -52,4 +79,3 @@ end
 #bubble_sort
 #a = [1,3,7,2,5]
 #p bubble_sort(a)
-

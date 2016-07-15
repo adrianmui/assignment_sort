44fae745f34b55fa231c2df441631ab390d9df49
jsteenb2 <jonathansteenbergen@gmail.com>
2016-07-15 09:30:44 -0700

    finished insertion_sort

--- sort.rb
+++ sort.rb
@@ -1,5 +1,5 @@
 def insertion_sort(arr)
-  sorted = []<< arr[0].shift
+  sorted = []<< arr.shift
   until arr.empty?
     insert(sorted, sorted.length - 1, arr.shift)
   end
@@ -7,13 +7,14 @@ def insertion_sort(arr)
 end
 
 #
-def insert(array, right_index, value)
+def insert(sorted_array, right_index, value)
   i = right_index
-  while(i >= 0 && array[i] > value)
-    array[i+1] = array[i]
+  while(i >= 0 && sorted_array[i] > value)
+    sorted_array[i+1] = sorted_array[i]
     i -= 1
   end
-  array[i+1] = value;
+  sorted_array[i+1] = value;
 end
 
-insertion_sort([1,3,2])
+p insertion_sort([1,3,2])
+p insertion_sort( [1,3,7,2,5] )
